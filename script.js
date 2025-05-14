f69d91ca1c4663ebc9dc4f84243619b6345b8183
fratur <francescoturco2795@gmail.com>
2025-05-14 19:45:42 +0200

    exercise-done

--- script.js
+++ script.js
@@ -8,19 +8,39 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+console.log (teachers)
+
+
+
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = [...teachers].reverse();
+console.log(reversedTeachers)
+
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(teacher => teacher.length >= 5);
+console.log(longNames)
+
+
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const removeName = teachers.filter(teacher => teacher.length > 2)
+console.log (removeName)
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log(isFabioPresent)
+
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(', ');
+console.log(teachersString)
